ca622a1813c858f3da538d2ff77bf7fe289d8565
xuwei <email.xuwei@gmail.com>
2020-11-23 00:05:57 +1100

    fixed unit tests

--- ozweatherTests/service/WeatherSearchCacheService/implementation/WeatherServiceCacheMockTests.swift
+++ ozweatherTests/service/WeatherSearchCacheService/implementation/WeatherServiceCacheMockTests.swift
@@ -14,11 +14,11 @@ class WeatherServiceCacheMockTests: XCTestCase {
     let cacheQueueName = "testQueue"
     
     func testEnqueueAndGetQeue() {
-        cache.clearList(listName: cacheQueueName)
+        cache.clearList(listName: .testSearchCacheList)
         let dummy = WeatherSearchCacheItem(city: "Atlanta", zipCode: nil, longitude: nil, latitude: nil, type: .city)
-        let queue1 = cache.enqueue(listName: cacheQueueName, element: dummy)
+        let queue1 = cache.enqueue(listName: .testSearchCacheList, element: dummy)
         XCTAssertTrue(queue1.count == 100)
-        let queue2 = cache.getQueue(listName: cacheQueueName) ?? []
+        let queue2 = cache.getQueue(listName: .testSearchCacheList) ?? []
         XCTAssertNotNil(queue2)
         XCTAssertTrue(queue2.count == 100)
     }
