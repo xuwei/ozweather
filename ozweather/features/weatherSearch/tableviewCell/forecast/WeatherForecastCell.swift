ff966d30207b0c041486f3402d6bf10743e3980c
xuwei <email.xuwei@gmail.com>
2020-11-23 11:17:38 +1100

    added placeholder for forecast cell

---
+++ ozweather/features/weatherSearch/tableviewCell/forecast/WeatherForecastCell.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherForecastCell.swift
+//  ozweather
+//
+//  Created by Xuwei Liang on 23/11/20.
+//
+
+import UIKit
+
+class WeatherForecastCell: UITableViewCell {
+    
+    static let identifier: String = "WeatherForecastCell"
+}
---
+++ ozweather/features/weatherSearch/tableviewCell/forecast/WeatherForecastCellVM.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherForecastCellVM.swift
+//  ozweather
+//
+//  Created by Xuwei Liang on 23/11/20.
+//
+
+import Foundation
+
+struct WeatherForecastCellVM: TableViewCellVMProtocol {
+    var identifier: String
+    
+
+}
